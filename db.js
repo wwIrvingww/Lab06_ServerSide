d7947999c610f02264b002327288922d22cf060b
Irving <mor22781@uvg.edu.gt>
2024-03-07 23:22:51 -0600

    eslint and cors added

--- db.js
+++ db.js
@@ -1,89 +1,89 @@
-import conn from './connection.js';
+import conn from './connection.js'
 
 export async function getAllBlogs() {
-    try {
-        const [rows] = await conn.query('SELECT * FROM blogs');
-        return rows;
-    } catch (e) {
-        console.error(e);
-        throw { status: 500, message: 'Error contacting the database or a code error occurred.' };
-    }
+  try {
+    const [rows] = await conn.query('SELECT * FROM blogs')
+    return rows
+  } catch (e) {
+    console.error(e)
+    throw { status: 500, message: 'Error contacting the database or a code error occurred.' }
+  }
 }
 
 export async function getBlog(id) {
-    try {
-        const [result] = await conn.query(`SELECT * FROM blogs WHERE id = ${id}`);
-        if (!result) {
-            throw { status: 400, message: 'Bad Request: Blog not found.' };
-        }
-        return result;
-    } catch (e) {
-        console.error(e);
-        if (e.status) {
-            throw e;
-        } else {
-            throw { status: 500, message: 'Error contacting the database or a code error occurred.' };
-        }
+  try {
+    const [result] = await conn.query(`SELECT * FROM blogs WHERE id = ${id}`)
+    if (!result) {
+      throw { status: 400, message: 'Bad Request: Blog not found.' }
     }
+    return result
+  } catch (e) {
+    console.error(e)
+    if (e.status) {
+      throw e
+    } else {
+      throw { status: 500, message: 'Error contacting the database or a code error occurred.' }
+    }
+  }
 }
 
 export async function createBlog(title, content) {
-    try {
-        if (!title || !content) {
-            throw { status: 400, message: 'Bad Request: Title and content are required.' };
-        }
-
-        const [result] = await conn.query(`INSERT INTO blogs (title, content) VALUES ('${title}', '${content}')`);
-        console.log(title, content);
-        return result;
-    } catch (e) {
-        console.error(e);
-        if (e.status) {
-            throw e;
-        } else {
-            throw { status: 500, message: 'Error contacting the database or a code error occurred.' };
-        }
+  try {
+    if (!title || !content) {
+      throw { status: 400, message: 'Bad Request: Title and content are required.' }
     }
+
+    const [result] = await conn.query(`INSERT INTO blogs (title, content) VALUES ('${title}', '${content}')`)
+    console.log(title, content)
+    return result
+  } catch (e) {
+    console.error(e)
+    if (e.status) {
+      throw e
+    } else {
+      throw { status: 500, message: 'Error contacting the database or a code error occurred.' }
+    }
+  }
 }
 
 export async function editBlog(id, newTitle, newContent) {
-    try {
-        if (!newTitle || !newContent) {
-            throw { status: 400, message: 'Bad Request: New title and content are required.' };
-        }
-
-        const [result] = await conn.query(`UPDATE blogs SET title = '${newTitle}', content = '${newContent}' WHERE id = ${id}`);
-        if (result.affectedRows === 0) {
-            throw { status: 400, message: 'Bad Request: Blog not found.' };
-        }
-        return result;
-    } catch (e) {
-        console.error(e);
-        if (e.status) {
-            throw e;
-        } else {
-            throw { status: 500, message: 'Error contacting the database or a code error occurred.' };
-        }
+  try {
+    if (!newTitle || !newContent) {
+      throw { status: 400, message: 'Bad Request: New title and content are required.' }
     }
+
+    const [result] = await conn.query(`UPDATE blogs SET title = '${newTitle}', content = '${newContent}' WHERE id = ${id}`)
+    if (result.affectedRows === 0) {
+      throw { status: 400, message: 'Bad Request: Blog not found.' }
+    }
+    return result
+  } catch (e) {
+    console.error(e)
+    if (e.status) {
+      throw e
+    } else {
+      throw { status: 500, message: 'Error contacting the database or a code error occurred.' }
+    }
+  }
 }
 
 export async function deleteBlog(id) {
-    try {
-        const [result] = await conn.query(`DELETE FROM blogs WHERE id = ${id}`);
-        if (result.affectedRows === 0) {
-            throw { status: 400, message: 'Bad Request: Blog not found.' };
-        }
-        return result;
-    } catch (e) {
-        console.error(e);
-        if (e.status) {
-            throw e;
-        } else {
-            throw { status: 500, message: 'Error contacting the database or a code error occurred.' };
-        }
+  try {
+    const [result] = await conn.query(`DELETE FROM blogs WHERE id = ${id}`)
+    if (result.affectedRows === 0) {
+      throw { status: 400, message: 'Bad Request: Blog not found.' }
     }
+    return result
+  } catch (e) {
+    console.error(e)
+    if (e.status) {
+      throw e
+    } else {
+      throw { status: 500, message: 'Error contacting the database or a code error occurred.' }
+    }
+  }
 }
 
 export function notImplemented() {
-    throw { status: 501, message: 'Not Implemented: This HTTP method is not supported.' };
+  throw { status: 501, message: 'Not Implemented: This HTTP method is not supported.' }
 }
--- index.js
+++ index.js
@@ -1,76 +1,73 @@
 import express from 'express'
-import { getAllBlogs, getBlog  ,createBlog, deleteBlog, editBlog, notImplemented} from './db.js'
-
-
-
+import cors from 'cors'
+import {
+  getAllBlogs, getBlog, createBlog, deleteBlog, editBlog, notImplemented,
+} from './db.js'
 
 const app = express()
 const port = 3000
 
-
+app.use(cors())
 app.use(express.json())
-
 
 app.get('/blogs', async (req, res) => {
   try {
-      const blogs = await getAllBlogs();
-      res.json(blogs);
+    const blogs = await getAllBlogs()
+    res.json(blogs)
   } catch (error) {
-      res.status(error.status || 500).json({ message: error.message });
+    res.status(error.status || 500).json({ message: error.message })
   }
-});
+})
 
 app.get('/blogs/:id', async (req, res) => {
-  const id = req.params.id;
+  const { id } = req.params
 
   try {
-      const blog = await getBlog(id);
-      res.json(blog);
+    const blog = await getBlog(id)
+    res.json(blog)
   } catch (error) {
-      res.status(error.status || 500).json({ message: error.message });
+    res.status(error.status || 500).json({ message: error.message })
   }
-});
+})
 
 app.post('/blogs', async (req, res) => {
-  const { title, content } = req.body;
+  const { title, content } = req.body
 
   try {
-      const result = await createBlog(title, content);
-      res.json(result);
+    const result = await createBlog(title, content)
+    res.json(result)
   } catch (error) {
-      res.status(error.status || 500).json({ message: error.message });
+    res.status(error.status || 500).json({ message: error.message })
   }
-});
+})
 
 app.put('/blogs/:id', async (req, res) => {
-  const id = req.params.id;
-  const { newTitle, newContent } = req.body;
+  const { id } = req.params
+  const { newTitle, newContent } = req.body
 
   try {
-      const result = await editBlog(id, newTitle, newContent);
-      res.json(result);
+    const result = await editBlog(id, newTitle, newContent)
+    res.json(result)
   } catch (error) {
-      res.status(error.status || 500).json({ message: error.message });
+    res.status(error.status || 500).json({ message: error.message })
   }
-});
+})
 
 app.delete('/blogs/:id', async (req, res) => {
-  const id = req.params.id;
+  const { id } = req.params
 
   try {
-      const result = await deleteBlog(id);
-      res.json(result);
+    const result = await deleteBlog(id)
+    res.json(result)
   } catch (error) {
-      res.status(error.status || 500).json({ message: error.message });
+    res.status(error.status || 500).json({ message: error.message })
   }
-});
+})
 
-// Manejar métodos no implementados
 app.all('/blogs', (req, res) => {
-  notImplemented();
-});
+  notImplemented()
+})
 
-// Escuchar en el puerto
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+  console.log(`Server is running on http://localhost:${port}`)
+})
